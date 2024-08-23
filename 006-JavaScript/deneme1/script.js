38304ab8a0239e7f725114b85bb28c4f8efd99ed
ufurkank <158720461+ufurkank@users.noreply.github.com>
2024-08-23 16:17:32 +0300

    if-else deneme yapıldı

---
+++ 006-JavaScript/deneme1/script.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+const userAge = 22 // prompt("TC No Giriniz: ")
+const userTC = 12345678922 //prompt("Yasinizi Giriniz: ")
+
+if (userAge >= 18 && String(userTC).length == 11) {
+    console.log("Oy Kullanabilir")
+} else {
+    console.log("Oy Kullanamaz")
+}
