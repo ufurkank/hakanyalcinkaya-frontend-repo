1f881a6dd5869f05efde636d10127d82c1851f00
ufurkank <158720461+ufurkank@users.noreply.github.com>
2024-08-28 13:44:02 +0300

    foreach ikili dropdown alıştırma yapıldı

---
+++ 006-JavaScript/deneme3/deneme.js
@@ -0,0 +1,17 @@
+
+//----------------------------------------------------//
+
+// forEach deneme1 Object
+const ufkObject = {
+    key1: "value1",
+    key2: "value2"
+}
+
+Object.keys(ufkObject).forEach(element => {
+    console.log(element)
+});
+
+// forEach deneme2 Array
+const ufkArray = ["Ali", "Veli", 4950]
+
+ufkArray.forEach((key) => console.log(key))
